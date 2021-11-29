6e1c281da30f04dd9942d4e0e1a6e7af06f2fa34
Shannon Hicks <shannonehiccups@gmail.com>
2021-11-28 20:54:04 -0600

    code along project up to episode 14

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  // const title = 'Welcome to the new blog!';
+  // const likes = 50; 
+  // const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        < Home />
+        {/* <p> Liked {likes} times</p>
+        <p>{ 10 }</p>
+        <p>"hello, ninjas"</p>
+        <p>[1,2,3]</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google</a> */}
+      </div>
     </div>
   );
 }
